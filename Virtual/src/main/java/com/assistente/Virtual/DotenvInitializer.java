d19ca0babf24045d1c6ec6976973a2f32a204ed1
Everton Alves <64697974+Everton543@users.noreply.github.com>
2024-12-03 19:20:01 -0300

    Ajustando o .env para funcionar em produção

--- Virtual/src/main/java/com/assistente/Virtual/DotenvInitializer.java
+++ Virtual/src/main/java/com/assistente/Virtual/DotenvInitializer.java
@@ -4,10 +4,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class DotenvInitializer {
     static {
+        String dotenvPath = "/etc/secrets/.env";
+
         Dotenv dotenv = Dotenv.configure()
+            .directory("/etc/secrets")
             .filename(".env")
             .load();
 
+        if (dotenv.entries().isEmpty()) {
+            throw new RuntimeException("Arquivo .env não encontrado no caminho: " + dotenvPath);
+        }
+        
         dotenv.entries().forEach(entry -> {
             System.setProperty(entry.getKey(), entry.getValue());
         });
